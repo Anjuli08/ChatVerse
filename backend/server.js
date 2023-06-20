dfea51bfa438410a87fa72f96231a0efee96d94a
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-06-21 01:20:10 +0530

    installed env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
 const app = express();
-
+dotenv.config();
 app.get('/', (req, res) => {
     res.send("API is running");
 
@@ -18,4 +19,6 @@ app.get('/api/chat/:id', (req, res) => {
     res.send(singleChat);
 })
 
-app.listen(5000, console.log("server started on port 5000"));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
