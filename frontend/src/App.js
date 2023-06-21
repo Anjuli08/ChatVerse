de98686e2bdf7a03d60737a16c372b58b3f3be47
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-06-21 11:27:57 +0530

    data access from server to client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import { Button } from "@chakra-ui/button";
 import './App.css';
+import {Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage';
 
 function App() {
   return (
   <div className = "App" >
-      
+      <Route path='/' component={Homepage} exact />
+      <Route path='/chats' component={Chatpage} />
   </div>
   );
 }
