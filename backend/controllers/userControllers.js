37bb1d14dab3ad6868a72eda4660a861564c7885
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-06-22 01:12:22 +0530

    authorization middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -58,4 +58,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+// /api/user/route
+// here data is send through query (?search=<name>)
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  //now provide it to mongodb:-
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(registerUser); //for registration
+router.route("/").post(registerUser).get(protect, allUsers); //for registration
 router.post("/login", authUser); //for login
+// router.route('/').get(allUsers)
 
 module.exports = router;
